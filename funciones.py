021f6f00f95094d06b163a70326d8bc252af7001
Sebastián Mora <sebastianmora93@gmail.com>
2020-07-29 18:48:50 -0500

    Revisando funciones

--- funciones.py
+++ funciones.py
@@ -1,4 +1,7 @@
 def imprimir_mensaje():
     print("Mensaje especial: ")
     print("¡Estoy aprendiendo a usar funciones!")
-imprimir_mensaje():
+
+imprimir_mensaje()
+imprimir_mensaje()
+imprimir_mensaje()
