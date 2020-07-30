584fea6f997f981fa3eb3ebf79f8eb90663c0450
Sebastián Mora <sebastianmora93@gmail.com>
2020-07-29 21:00:37 -0500

    Modulo conversor

--- condicionales.py
+++ condicionales.py
@@ -17,11 +17,11 @@ Elige una opción: """
 
 opcion = int(input(menu))
 
-if opcion == "1":
+if opcion == 1:
     conversor("colombianos", 3875)
-elif opcion == "2":
+elif opcion == 2:
     conversor("argentinos", 65)
-elif opcion == "3":
+elif opcion == 3:
     conversor("mexicanos", 24)
 else:
     print("Ingresa una opción correcta por favor")
