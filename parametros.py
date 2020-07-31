1db50476d2cedffe85d196224eeeff9bd8d2f12a
Sebastián Mora <sebastianmora93@gmail.com>
2020-07-31 10:39:13 -0500

    Finalizado y probado programa parámetros

--- parametros.py
+++ parametros.py
@@ -10,23 +10,32 @@ def calculo_antojo(salario1, valor_antojo1, compras_previas1, valor_compras_prev
     elif compras_previas1 == "Sí":
         if valor_compras_previas1 >= valor_antojo1:
             print("No compres ese antojo, ya ha sido suficiente por ahora.")
+        elif relevancia_antojo1 <= 2:
+            print("El antojo no vale la pena, no lo hagas.")
+        elif relevancia_antojo1 <= 4:
+            print("Puedes comprar el antojo pero piénsalo muy bien: quizás no es lo mejor ahora mismo.")
+        elif relevancia_antojo1 > 4:
+            print("Está bien, de verdad quieres ese antojo, cómpralo.")
+        else:
+            print("Hiciste algo mal, por favor vuelve a empezar y asegúrate de escribir todo bien.")
     elif relevancia_antojo1 <= 2:
         print("El antojo no vale la pena, no lo hagas.")
     elif relevancia_antojo1 <= 4:
-        print("Puedes comprar el antojo pero piénsalo muy bien: quizás no es lo mejor ahora mismo.")
+        print("Puedes comprar el antojo pero piénsalo muy bien, quizás no es lo mejor ahora mismo.")
     elif relevancia_antojo1 > 4:
         print("Está bien, de verdad quieres ese antojo, cómpralo.")
     else:
         print("Hiciste algo mal, por favor vuelve a empezar y asegúrate de escribir todo bien.")
 
-salario = int(input("Por favor digita tu salario mensual (sólo números, no incluyas decimales): "))
+salario = int(input("Digita tu salario mensual (sólo números, no incluyas decimales): "))
 valor_antojo = int(input("Ahora ingresa el valor del antojo que deseas comprar (sólo números, no incluyas decimales): "))
 compras_previas = input("¿Has comprado antojos en los últimos 30 días? (Responde 'Sí' o 'No'): ")
 
 if compras_previas == "Sí":
     valor_compras_previas = int(input("¿Cuál fue el valor de ese antojo? (sólo números, no incluyas decimales): "))
-# return valor_compras_previas
+else:
+    valor_compras_previas = 0
 
-relevancia_antojo = int(input("En una escala de 1 a 5, donde 1 equivale a 'insignificante' y 5 a 'esencial', ¿cómo calificarías tu deseo por este anotjo?: "))
+relevancia_antojo = int(input("En una escala de 1 a 5, donde 1 equivale a 'insignificante' y 5 a 'esencial', ¿cómo calificarías tu deseo por este antojo?: "))
 
 calculo_antojo(salario, valor_antojo, compras_previas, valor_compras_previas, relevancia_antojo)
