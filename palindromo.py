d123b18d50edd938b75b23e35e2d72a6f63e464c
Sebastián Mora <sebastianmora93@gmail.com>
2020-07-31 19:37:10 -0500

    Primer bucle while finalizado

--- palindromo.py
+++ palindromo.py
@@ -2,7 +2,7 @@ def palindromo(palabra):
     palabra = palabra.replace(" ", "")
     palabra = palabra.lower()
     palabra_invertida = palabra[::-1]
-    if palabra = palabra_invertida:
+    if palabra == palabra_invertida:
         return True
     else:
         return False
