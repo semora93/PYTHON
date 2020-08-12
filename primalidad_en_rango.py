fc68068df24d0a20e5914ad2671d808c188eb4b8
Sebastián Mora <sebastianmora93@gmail.com>
2020-08-11 21:31:41 -0500

    nueva primalidad

--- primalidad_en_rango.py
+++ primalidad_en_rango.py
@@ -1,5 +1,5 @@
 def run():
-    print("Hola, este programa te ayudará a realizar una pruba de primalidad para todos los números en un intervalo determinado.")
+    print("Hola, este programa te ayudará a identificar todos los números primos dentro de un rango determinado.")
     comienzo_rango = int(input("Ingresa el número entero que denota el comienzo del rango deseado: "))
     final_rango = int(input("Ahora ingresa el número entero que denota el final del rango deseado (el rango incluirá este número): "))
     rango_usuario = list(range(comienzo_rango, final_rango + 1))
@@ -8,14 +8,11 @@ def run():
         while div > 1:
             resid = i % div
             if resid == 0:
-                print(str(i) + " no es primo")
                 break
             elif resid != 0:
                 div -= 1
         if div == 1:
-            print(str(i) + " es primo")
-        elif div == 0:
-            print(str(i) + " no es primo")
+            print(i)            
 
 
 if __name__ == "__main__":
