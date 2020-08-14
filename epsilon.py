026183d643f0cff36e2c01acd60ef42cde37f86f
Sebastián Mora <sebastianmora93@gmail.com>
2020-08-14 17:06:55 -0500

    pruebas con epsilon

--- epsilon.py
+++ epsilon.py
@@ -4,9 +4,10 @@ paso = epsilon**2
 respuesta = 0.0
 
 while abs(respuesta**2 - objetivo) >= epsilon and respuesta <= objetivo:
+    print(respuesta)
     respuesta += paso
 
 if abs(respuesta**2 - objetivo) >= epsilon:
     print(f"No se encontró la raíz cuadrada de {objetivo}")
 else:
-    print(f"La raíz cuadrada de {objetivo} es cercana {respuesta}")
+    print(f"La raíz cuadrada de {objetivo} es cercana a {respuesta}")
