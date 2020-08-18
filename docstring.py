7ad8b37894ba8482644f255ad57a78e9197d7d09
Sebastián Mora <sebastianmora93@gmail.com>
2020-08-18 16:59:55 -0500

    prueba scope y especificaciones

---
+++ docstring.py
@@ -0,0 +1,6 @@
+def a(cualquier_parametro):
+    """Descripción de lo que hace nuestra función
+    cualquier_parámetro int cualquier entero
+    return el cualquier_parámetro + 5
+    """
+    return cualquier_parametro + 5
